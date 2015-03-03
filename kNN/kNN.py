960787de91f08750509333bcfde22007c5f5dbcd
tianling <2507073658@qq.com>
2015-03-03 23:30:41 +0800

    file work

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -11,10 +11,14 @@ def createDataSet():
 
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
+
+    #计算待分类点和训练集中任一点的欧式距离
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances**0.5
+
+    #排序和统计
     sortedDistIndicies = distances.argsort()
     classCount = {}
     for i in range(k):
@@ -22,4 +26,26 @@ def classify0(inX, dataSet, labels, k):
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
+
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOlines = fr.readlines()
+    lineNum = len(arrayOlines) #得到文件行数
+    returnMat = zeros((lineNum,3))
+    classLabelVector = []
+    index = 0
+    #解析文件数据到列表
+    for line in arrayOlines:
+        line = line.strip()
+        listFromLine = line.split('/t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine(-1)))
+        index += 1
+
+    return returnMat,classLabelVector
+
+
+
 
